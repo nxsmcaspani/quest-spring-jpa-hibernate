91ff48704ab61b0fe9cf3713215a34d6b1e6b5dc
Maxime Caspani <maxime.caspani@natixis.com>
2021-05-27 10:32:43 +0200

    Quest Completed

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,28 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-// TODO : update this interface
+import com.wildcodeschool.wildandwizard.entity.School;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SchoolRepository {
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long> {
+
+    @Override
+    List<School> findAll(Sort sort);
+
+    @Override
+    <S extends School> S save(S s);
+
+    @Override
+    void deleteById(Long aLong);
+
+    @Override
+    default Optional<School> findById(Long aLong) {
+        return Optional.empty();
+    }
+
 }
